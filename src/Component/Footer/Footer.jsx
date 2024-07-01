f4e58337e99b1072e914fabc37cccb534caf87a0
Farzad_Sarfaraz <129787719+Farzadsarfaraz@users.noreply.github.com>
2024-07-01 20:09:40 +0200

    New update

--- src/Component/Footer/Footer.jsx
+++ src/Component/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import './Footer.css'
 const Footer = () => {
   return (
     <div className='footer'>
-        <p>2024 Coy right</p>
+        <p>2024 Copy right</p>
         <ul>
             <li>Term of Serivece</li>
             <li>Privacy policy</li>
